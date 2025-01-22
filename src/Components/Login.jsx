7e887294a06c0219261e3bb94c4017238e6da50d
Tatianna <tatirobinson@gmail.com>
2025-01-21 22:52:46 -0800

    Create login method

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,12 +3,36 @@ import { useState } from 'react';
 
 const Login = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
 
-    const login = () => {
+    const login = async (e) => {
 
-        
+        e.preventDefault();
+
+        const url = 'https://frontend-take-home-service.fetch.com/auth/login';
+
+        try {
+                const res = await fetch(url, {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    method: "POST",
+                    body: JSON.stringify({name: userName, email: userEmail})
+                }
+            );
+
+            if (!Response.ok){
+                throw new Error(res.status);
+                console.log(res);
+            }
+
+            const json = await res.json();
+            console.log(json);
+
+        } catch (error){
+            console.log(error);
+        }
     }
 
     return (
@@ -16,8 +40,8 @@ const Login = () => {
             <h1> Login </h1>
 
             <form onSubmit={login}>
-                <input type='text' placeholder='Name'></input>
-                <input type='email' placeholder='Email'></input>
+                <input type='text' placeholder='Name'onChange={(e) => setUserName(e.target.value)}></input>
+                <input type='email' placeholder='Email' onChange={(e) => setUserEmail(e.target.value)}></input>
                 <button>Login</button>
             </form>
         </>
