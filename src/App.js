7d2040864a18eb84d61f66dcb8f839b491a8bccb
Tatianna <tatirobinson@gmail.com>
2025-01-23 23:00:41 -0800

    Create router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-import Login from './Components/Login';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import { createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: < Login />
+  },
+
+  {
+    path: '/search',
+    element: < Home/>
+  },
+
+  {
+    path: "/*",
+    element: < Navigate to='/'/>
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
