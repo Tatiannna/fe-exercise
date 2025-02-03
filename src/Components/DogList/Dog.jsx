645caddb07130dd89d5a8d98bca65e27d7bf2586
Tatianna <tatirobinson@gmail.com>
2025-02-02 21:01:33 -0800

    Add basic styling to Dog components

--- src/Components/DogList/Dog.jsx
+++ src/Components/DogList/Dog.jsx
@@ -4,14 +4,12 @@ import './Dog.css';
 const Dog = ({dog}) => {
     return(
         <>
-            <div className='dog-list-container'>
-                <div className='dog-container'>
-                    <p>{dog.name}</p>
-                    <p>{dog.breed}</p>
-                    <p>{dog.age}</p>
-                    <p>{dog.zipcode}</p>
-                    <img src={dog.img} alt=""/>
-                </div>
+            <div className='dog-container'>
+                <p>{dog.name}</p>
+                <p>{dog.breed}</p>
+                <p>{dog.age}</p>
+                <p>{dog.zipcode}</p>
+                <img src={dog.img} alt=""/>
             </div>
         </>
     );
--- src/Components/DogList/DogList.jsx
+++ src/Components/DogList/DogList.jsx
@@ -1,26 +1,26 @@
-import { useDispatch, useSelector } from 'react-redux';
 import './DogList.css';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { searchDogs } from '../../store/dogs';
+import Dog from './Dog';
+
+
 
 const DogList = () => {
 
     const dispatch = useDispatch();
-    const dogs = useSelector(state => state.dogs)
-
+    const dogs = useSelector(state => state.dogs);
 
     useEffect( () => {
         dispatch(searchDogs());
-    }, [dispatch])
-    console.log(dogs);
+    }, [dispatch]);
 
     return (
         <>
-            <ul>List of Dogs</ul>
-
-                {dogs.resultIds.forEach(element => {
-                    <li>element</li>
-                })}
+            <h4>List of Dogs</h4>
+            <div className='dog-list-container'>
+                {Object.values(dogs).map((element) => <Dog dog={element}/>)}
+            </div>
         </>
     );
 }  
