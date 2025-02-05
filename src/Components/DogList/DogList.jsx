c0eeaa983637edccb25257f745cb07728ad4f8ed
Tatianna <tatirobinson@gmail.com>
2025-02-04 18:58:52 -0800

    Add, style paginate component

--- src/Components/DogList/DogList.jsx
+++ src/Components/DogList/DogList.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { searchDogs } from '../../store/dogs';
 import Dog from './Dog';
+import ReactPaginate from 'react-paginate';
+import './ReactPaginate.css';
 
 
 
@@ -10,10 +12,15 @@ const DogList = () => {
 
     const dispatch = useDispatch();
     const dogs = useSelector(state => state.dogs);
+    const [pageCount, setPageCount] = useState(10);
 
     useEffect( () => {
         dispatch(searchDogs());
     }, [dispatch]);
+
+    const handlePageClick = () => {
+        console.log("clicked");
+    }
 
     return (
         <>
@@ -21,6 +28,19 @@ const DogList = () => {
             <div className='dog-list-container'>
                 {Object.values(dogs).map((element) => <Dog dog={element}/>)}
             </div>
+
+            <ReactPaginate
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount} 
+                marginPagesDisplayed={2} // Number of pages to show at the beginning and end
+                pageRangeDisplayed={3} // Number of pages to show around the current page
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+            />
         </>
     );
 }  
